46733ccf51a2baa80db9c1aa26024b07622504ea
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-08-02 19:46:08 -0700

    First cut for basic infrastructure

---
+++ lib/podge.ml
@@ -0,0 +1,23 @@
+(** Shortcuts and helpers for common tasks in OCaml *)
+
+module Math = struct
+
+end
+
+module Json = struct
+
+
+end
+
+
+module List = struct
+
+end
+
+module Html = struct
+
+end
+
+module Js = struct
+
+end
