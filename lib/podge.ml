8823a8254d7a82afec637f7325f2ae0993cbfcd0
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-08-02 20:10:08 -0700

    Continue setup of build infrastructure

--- lib/podge.ml
+++ lib/podge.ml
@@ -21,3 +21,19 @@ end
 module Js = struct
 
 end
+
+module Unix = struct
+
+  let read_process_output p =
+    let ic = Unix.open_process_in p in
+    let all_input = ref [] in
+    try
+      while true do
+        all_input := input_line ic :: !all_input;
+      done;
+      []
+    with
+      End_of_file ->
+      close_in ic;
+      !all_input
+end
