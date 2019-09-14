3142e417545ba90c959e0f181d36e62f53b588cc
zapashcanon <leo@ndrs.fr>
2019-09-15 00:19:37 +0200

    switch to dune (#8)

--- tests/do_tests.ml
+++
@@ -1,7 +0,0 @@
-#require "podge"
-
-let () =
-  (* Podge.Web.get Sys.argv.(1) *)
-  (* |> Podge.Yojson.show_pretty_of_in_mem *)
-  Podge.Web.put Sys.argv.(1) Sys.argv.(2)
-  |> Podge.Yojson.show_pretty_of_in_mem
